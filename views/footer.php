1bd42ef4c5873a38ec3d1db675291c458a80fea5
Maria Gustafsson Niså <maria.gnisa@gmail.com>
2016-11-07 15:21:28 +0100

    added som htm and im
    Former-commit-id: fe31e73bdf292aa3492dfa7bdfbf2220397ed282

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,9 @@
 <footer>
-Group 9
+<div class="footer">
+  <p> Adress: Kaserntorget 6, 411 18 Göteborg </p>
+  <p> Tel: 031 105 490 </p>
+  <p> e-mail: contact@milega.se </p>
+</div>
 </footer>
 
 
--- views/header.php
+++ views/header.php
@@ -2,27 +2,26 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <title>Milega</title>
     <link href="./assets/css/normalize.css" rel="stylesheet" />
     <link href="./assets/css/style.css" rel="stylesheet" />
   </head>
   <body>
 <header>
   <nav class="nav">
-    <h1>Milega</h1>
+  <img src="./assets/images/milegalogo.png" alt="Logotype" />
     <ul class="nav">
-      <li><a href="/">Link1<a></li>
-      <li><a href="/">Link2<a></li>
-      <li><a href="/">Link3<a></li>
+      <li><a href="/">Kursmaterial<a></li>
+      <li><a href="/">Tester<a></li>
+      <li><a href="/">Föreläsningar<a></li>
     </ul>
     <div class="login">
       <span>Login</span>
       <div class="login-wrapper">
-        <form class="login-form">
-          <label>Email</label>
-          <input type="email" />
-          <label>Password</label>
-          <input type="password" />
+        <form class="login-form" action="/login" method="post">
+          <input type="email" placeholder="Din email" />
+          <input type="password" placeholder="Lösenord"/>
+          <input type="button" name="submit" value="Logga in">
         </form>
       </div>
     </div>
