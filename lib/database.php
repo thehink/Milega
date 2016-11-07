f842ce9112c81993cf169c2f9908b1cc0b317bd5
thehink <benja280@gmail.com>
2016-11-07 15:20:00 +0100

    Beginning of login system

---
+++ lib/database.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'flight/Flight.php';
+require_once 'user.model.php';
+
+Flight::register('db', 'PDO', array('sqlite:db/milega.sqlite');
+
+
+/**
+ *
+ */
+class Database
+{
+
+}
--- lib/header.php
+++ lib/header.php
@@ -1,2 +1,7 @@
 <?php
 session_start();
+require_once 'flight/Flight.php';
+require_once 'database.php';
+require_once 'authentication.php';
+
+Authentication::checkLogin();
