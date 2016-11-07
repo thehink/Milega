0a0d386098308fce586f55e090b0e559c7ccb6f9
thehink <benja280@gmail.com>
2016-11-07 15:51:41 +0100

    Added register page
    Former-commit-id: fd4febe66cbb6ad341e8895644ade3c6bd556dbf

--- index.php
+++ index.php
@@ -14,11 +14,22 @@ Flight::route('/test', function(){
     Flight::render( 'footer', []);
 });
 
+Flight::route('GET /register', function(){
+  Flight::render( 'header', []);
+  Flight::render( 'register', []);
+  Flight::render( 'footer', []);
+});
 
 Flight::route('POST /login', function(){
   $request = Flight::request();
   print_r($request);
 });
+
+Flight::route('POST /register', function(){
+  $request = Flight::request();
+  print_r($request);
+});
+
 
 
 Flight::start();
