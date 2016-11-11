8cec5712dc869d84ab6bbabe6f3e1f4073fad3cf
thehink <benja280@gmail.com>
2016-11-11 11:12:37 +0100

    Removed print_r($user)
    Former-commit-id: ca75a51cce70fd04e46280a2199bf59769a9f524

--- views/index.php
+++ views/index.php
@@ -1,5 +1,5 @@
 <div class="content">
-  <h2>Välkommen <?=print_r(Flight::get('user'))?> (inloggad sida?)</h2>
+  <h2>Välkommen (inloggad sida?)</h2>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
 
 </div>
