360314d92c23d07a06a9639daf70874d7d8905e9
Thehink <benja280@gmail.com>
2016-11-12 12:41:25 +0100

    Removed echo used for debugging
    Former-commit-id: d643f0055d066f631d2d4d80824d76b77ec86ade

--- lib/pages.php
+++ lib/pages.php
@@ -6,7 +6,6 @@ class Pages
 {
   public static function __callStatic($method_name, $args) {
      require_once "pages/$method_name.php";
-
      if(in_array($_SERVER['REQUEST_METHOD'], ['POST', 'GET', 'PUT', 'DELETE'])){
        try{
          call_user_func_array([$method_name, $_SERVER['REQUEST_METHOD']], $args);
@@ -15,8 +14,5 @@ class Pages
         call_user_func_array([$method_name, 'render']);
        }
      }
-
-     echo 'Calling method ',$method_name,'<br />';
-     print_r($args);
   }
 }
