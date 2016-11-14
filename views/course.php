4afbc265bb7cc00c3937d63c08a6601e1e5041d1
Maria <maria.gnisa@gmail.com>
2016-11-14 21:37:32 +0100

    added and styled day 1 form on course page
    Former-commit-id: e52deb9428f7eb644b2884e4a4f37b5bc27be5d0

--- views/course.php
+++ views/course.php
@@ -3,6 +3,18 @@
     <h1>Dag 1</h1>
     <h2>Målet med mitt ledarskap</h2>
     <div class="content-form">
+      <p>
+        Det här vill jag med mitt ledarskap:
+      </p>
+      <textarea></textarea>
+      <p>
+        Vad vill jag åstadkomma med mitt ledarskap:
+      </p>
+      <textarea></textarea>
+      <p>
+        Vilka konkreta saker måste jag ta tag i (lösa, utveckla, förbättra):
+      </p>
+      <textarea></textarea>
 
     </div>
 
