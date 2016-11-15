509b7fea272ec926cf4a9ed74fef2d5aadc4254a
Maria <maria.gnisa@gmail.com>
2016-11-15 14:33:00 +0100

    course page under process
    Former-commit-id: e3c2bf3d22f32f2a7a5a52d2e3fcfc0432031a82

--- views/course.php
+++ views/course.php
@@ -6,14 +6,24 @@
       <p>
         Det här vill jag med mitt ledarskap:
       </p>
+      <div class="button-day-form1">
+        <a href="#"></a>
+
+      </div>
       <textarea></textarea>
       <p>
         Vad vill jag åstadkomma med mitt ledarskap:
       </p>
+      <div class="button-day-form2">
+        <a href="#"></a>
+      </div>
       <textarea></textarea>
       <p>
         Vilka konkreta saker måste jag ta tag i (lösa, utveckla, förbättra):
       </p>
+      <div class="button-day-form3">
+        <a href="#"></a>
+      </div>
       <textarea></textarea>
 
     </div>
