b0ae52210b22a3cde8fc380d849bc83a2101f8ef
thehink <benja280@gmail.com>
2016-11-16 11:59:33 +0100

    Started on profile graph page
    Former-commit-id: 903c14a110b3c5d6b21c4767bd009ceabd3cc161

---
+++ views/profileGraph.php
@@ -0,0 +1,12 @@
+<div class="content">
+  <h1>Simon Schultz</h1>
+  <div class="content-box porifle-graph">
+
+  </div>
+  <div class="content-box jti-result">
+    <p>I ledarskapsgrafen har vi föreslagit några olika områden som är viktiga att arbeta med i ledarskap.Se Y-axelns botten som 0 och toppen som 10. Gradera din tillfredställelse med varje förmåga på dess område i grafen och dra en ny linje. Den linjen representerar din personliga ledarskapsgraf.
+ </p>
+<p>Hur ser det ut? Hur skulle du vilja att det var? Vilket område vill du förändra först?
+</p>
+  </div>
+</div>
