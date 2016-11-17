8679897d19a0bf0ec2edcb0eac4262eec67943c3
thehink <benja280@gmail.com>
2016-11-17 15:04:40 +0100

    Dont show forgot password form when mail was sent

--- views/forgotPassword.php
+++ views/forgotPassword.php
@@ -1,5 +1,6 @@
 <div class="content">
   <div class="modal-form form-wrapper">
+    <?php if(!Flight::has('password.success')) : ?>
     <form action="/forgot_password" method="POST">
       <input type="email" placeholder="<?=Localization::get('EMAIL')?>" name="email" value="<?=$data['email'] ?? ''?>"/>
       <?=Flight::getError('password.form.error.email')?>
@@ -7,7 +8,8 @@
         <button type="submit"><?=Localization::get('REQUEST_NEW_PASSWORD')?></button>
       </div>
       <?=Flight::getError('password.error')?>
-      <?=Flight::getSuccess('password.success')?>
     </form>
+  <?php endif; ?>
+    <?=Flight::getSuccess('password.success')?>
   </div>
 </div>
