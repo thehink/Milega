c7e6cea4f236166b3206f9bfeec072121ba870d2
thehink <benja280@gmail.com>
2016-11-17 11:17:07 +0100

    Default config in config.default.php override config with config/config.php

---
+++ config/config.default.php
@@ -0,0 +1,11 @@
+<?php
+
+const _TOKEN_ENCRYPTION_ALG_ = 'aes-256-cbc';
+const _TOKEN_ENCRYPTION_KEY_ = 'utUgQiEi9RfVC9QNVXt4qqNdUBdfNSt1LZd4GkjVU13m7Y4MrXOKIaNrNvsplu+fiokQuz3SOTBCJLdwTWkaTA==';
+const _TOKEN_ENCRYPTION_IV_ = 'k4ZBxyGeqAfyQWj1wbW2iQ==';
+
+const _LOCALIZATION_DEFAULT_ = 'swedish';
+
+const _AUTHENTICATION_TOKEN_LIFETIME_ = 60 * 60 * 24 * 30;
+
+const _PDO_DATABASE_ = ['sqlite:db/milega.sqlite'];
--- lib/autoload.php
+++ lib/autoload.php
@@ -5,7 +5,13 @@ date_default_timezone_set('UTC');
 
 mb_internal_encoding('UTF-8');
 
-require_once './config/config.php';
+$config_path = __DIR__ . '/../config/config.php';
+if(file_exists($config_path)){
+  require_once $config_path;
+}else{
+  require_once './config/config.default.php';
+}
+
 require_once 'vendor/flight/flight/Flight.php';
 require_once 'database.php';
 require_once 'authentication.php';
