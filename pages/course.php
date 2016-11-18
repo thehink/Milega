68ef69d00be20fa0d7dfda247276ddb2302a72b4
Thehink <benja280@gmail.com>
2016-11-18 20:46:57 +0100

    Made days selectable & added an arrow on bottom with link to next day

--- pages/course.php
+++ pages/course.php
@@ -7,38 +7,40 @@ class Course
 {
 
   public static $assets = [
+    '<link href="/assets/css/guide.css" rel="stylesheet" />',
     '<link href="/assets/css/course.css" rel="stylesheet" />'
   ];
 
-  public static function render($questions){
+  public static function render($day = 1, $questions = []){
     Flight::display( 'course', [
       'assets' => self::$assets,
       'page' => 'course',
+      'day' => $day,
       'questions' => $questions,
       'subHeader' => [
         'Dag 1' => [
           'url' => '/course/1',
-          'selected' => true
+          'selected' => $day === 1
         ],
         'Dag 2' => [
-          'url' => '/course/1',
-          'selected' => false
+          'url' => '/course/2',
+          'selected' => $day === 2
         ],
         'Dag 3' => [
-          'url' => '/course/1',
-          'selected' => false
+          'url' => '/course/3',
+          'selected' => $day === 3
         ],
         'Dag 4' => [
-          'url' => '/course/1',
-          'selected' => false
+          'url' => '/course/4',
+          'selected' => $day === 4
         ],
         'Dag 5' => [
-          'url' => '/course/1',
-          'selected' => false
+          'url' => '/course/5',
+          'selected' => $day === 5
         ],
         'Dag 6' => [
-          'url' => '/course/1',
-          'selected' => false
+          'url' => '/course/6',
+          'selected' => $day === 6
         ]
       ]
     ]);
@@ -46,16 +48,17 @@ class Course
 
   public static function get($day){
     Authentication::requireLoggedIn();
-    $day = $day ?? 1;
+    $day = (int)($day ?? 1);
 
     $userId = Flight::get('user')->id;
 
     $questions = Questions::get($day);
+
     foreach ($questions as $question) {
       $question->answer = Answer::get($userId, $question->id);
     }
 
-    self::render($questions);
+    self::render($day, $questions);
   }
 
   public static function post(){
--- views/course.php
+++ views/course.php
@@ -1,6 +1,6 @@
 <div class="content">
   <div class="content-course">
-    <h1>Dag 1</h1>
+    <h1>Dag <?=$day?></h1>
     <h2>Målet med mitt ledarskap</h2>
     <div class="content-form">
       <?php foreach ($questions as $question) : ?>
@@ -14,6 +14,10 @@
         <textarea id="text<?=$question->id?>" class="textarea" disabled="true"><?=$question->answer ? $question->answer->answer : ''?></textarea>
         <button id="button<?=$question->id?>" class="saveButton" type="submit" name="saveButton" onclick="closeTextarea('text<?=$question->id?>', 'button<?=$question->id?>')">SPARA</button>
       <?php endforeach; ?>
+      <div class="guide-nav-buttons">
+        <a class="left-arrow hidden" href="/guide/intro"></a>
+        <a class="right-arrow" href="/course/2"></a>
+      </div>
     </div>
 
   </div>
