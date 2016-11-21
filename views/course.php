5a51d0a7dde9308a0a6f4922f7b9d6d4d329d423
Maria <maria.gnisa@gmail.com>
2016-11-21 13:59:49 +0100

    added content to course page day 2

--- views/course.php
+++ views/course.php
@@ -1,8 +1,30 @@
 <div class="content">
   <div class="content-course">
     <h1>Dag <?=$day?></h1>
-    <h2>Målet med mitt ledarskap</h2>
+    <h2><? if ($day == 1): ?> Målet med mitt ledarskap <? endif; ?>
+        <? if ($day == 2): ?> Mina styrkor och utvecklingsområden <? endif; ?>
+    </h2>
     <div class="content-form">
+
+      <? if ($day ==2) ?>
+      <p>
+        Detta är första övningen du gör i att färdigställa målet med ditt ledarskap.
+        Dokumentet kommer att vara levande under hela utbildningen och det är meningen att du skall fylla på allteftersom du tillför mer kunskaper om dig själv och vad du vill göra i din roll som ledare.
+        Dag 7 kommer du att inför gruppen kort få redogöra för hur långt ditt arbete med detta kommit.
+      </p>
+      <br>
+      <p>
+        Denna första öving är uppdelad i tre olika moment. I den första individuella övningen reflkterar du över din JTI. I den andra individuella övningen kartlägger du dig själv utifrån ditt uppdrag.
+      </p>
+      <br>
+      <p>
+        Därefter skall du i del 3 dels arbeta med en individuell kompetensprofil samt- i anslutning till denna- träffa en person i gruppen som skall "coacha" dig så att du får feedback på dina specifika kompetensområden.
+      </p>
+      <br>
+      <h3 class="bold">Mina styrkor och svagheter utifrån min JTI-profil</h3>
+
+      <h2 class="bold">Visa min JTI-profil</h2>
+
       <?php foreach ($questions as $question) : ?>
       <p>
         <?=$question->question?>
