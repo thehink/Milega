3bccff169a91011dc329fdf148d9b3c8f582ec7f
thehink <benja280@gmail.com>
2016-11-21 10:54:07 +0100

    Added graph to profile page

--- pages/profileGraph.php
+++ pages/profileGraph.php
@@ -6,10 +6,16 @@
 class ProfileGraph
 {
 
+  static $assets = [
+    '<link href="/assets/css/profile.css" rel="stylesheet" />',
+    '<link href="/assets/css/profileGraph.css" rel="stylesheet" />'
+  ];
+
   public static function render(){
     Flight::display( 'profileGraph', [
       'title' => 'PROFILE',
       'page' => 'profile',
+      'assets' => self::$assets,
       'subHeader' => [
         'JIT_TEST' => [
           'url' => '/profile',
--- views/profileGraph.php
+++ views/profileGraph.php
@@ -1,7 +1,9 @@
 <div class="content">
   <h1><?=Flight::get('user')->getFullName()?></h1>
-  <div class="content-box porifle-graph">
+  <div class="content-box">
+    <div class="profile-graph">
 
+    </div>
   </div>
   <div class="content-box">
     <p>I ledarskapsgrafen har vi föreslagit några olika områden som är viktiga att arbeta med i ledarskap.Se Y-axelns botten som 0 och toppen som 10. Gradera din tillfredställelse med varje förmåga på dess område i grafen och dra en ny linje. Den linjen representerar din personliga ledarskapsgraf.
