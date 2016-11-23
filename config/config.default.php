dbebae3cd9c9703a8666425d4074caeaed60f28b
thehink <benja280@gmail.com>
2016-11-23 11:16:26 +0100

    Fixed default settings

--- config/config.default.php
+++ config/config.default.php
@@ -1,19 +1,52 @@
 <?php
-const _DOMAIN_ = 'milega.benjar.net';
+if(!defined('_DOMAIN_')){
+  define('_DOMAIN_', 'milega.benjar.net');
+}
 
-const _TOKEN_ENCRYPTION_ALG_ = 'aes-256-cbc';
-const _TOKEN_ENCRYPTION_KEY_ = 'utUgQiEi9RfVC9QNVXt4qqNdUBdfNSt1LZd4GkjVU13m7Y4MrXOKIaNrNvsplu+fiokQuz3SOTBCJLdwTWkaTA==';
-const _TOKEN_ENCRYPTION_IV_ = 'k4ZBxyGeqAfyQWj1wbW2iQ==';
+if(!defined('_TOKEN_ENCRYPTION_ALG_')){
+  define('_TOKEN_ENCRYPTION_ALG_', 'aes-256-cbc');
+}
 
-const _LOCALIZATION_DEFAULT_ = 'swedish';
+if(!defined('_TOKEN_ENCRYPTION_KEY_')){
+  define('_TOKEN_ENCRYPTION_KEY_', 'utUgQiEi9RfVC9QNVXt4qqNdUBdfNSt1LZd4GkjVU13m7Y4MrXOKIaNrNvsplu+fiokQuz3SOTBCJLdwTWkaTA==');
+}
 
-const _AUTHENTICATION_TOKEN_LIFETIME_ = 60 * 60 * 24 * 30;
+if(!defined('_TOKEN_ENCRYPTION_IV_')){
+  define('_TOKEN_ENCRYPTION_IV_', 'k4ZBxyGeqAfyQWj1wbW2iQ==');
+}
 
-const _PDO_DATABASE_ = ['sqlite:db/milega.sqlite'];
+if(!defined('_LOCALIZATION_DEFAULT_')){
+  define('_LOCALIZATION_DEFAULT_', 'swedish');
+}
 
-const _EMAIL_ACTIVATED_ = false;
-const _EMAIL_SMTP_HOST_ = 'smtp.gmail.com';
-const _EMAIL_SMTP_PORT_ =  465;
-const _EMAIL_SMTP_SECURITY_ = 'ssl';
-const _EMAIL_SMTP_USERNAME_ = '';
-const _EMAIL_SMTP_PASSWORD_ = '';
+if(!defined('_AUTHENTICATION_TOKEN_LIFETIME_')){
+  define('_AUTHENTICATION_TOKEN_LIFETIME_', 60 * 60 * 24 * 30);
+}
+
+if(!defined('_PDO_DATABASE_')){
+  define('_PDO_DATABASE_', ['sqlite:db/milega.sqlite']);
+}
+
+if(!defined('_EMAIL_ACTIVATED_')){
+  define('_EMAIL_ACTIVATED_', false);
+}
+
+if(!defined('_EMAIL_SMTP_HOST_')){
+  define('_EMAIL_SMTP_HOST_', 'smtp.gmail.com');
+}
+
+if(!defined('_EMAIL_SMTP_PORT_')){
+  define('_EMAIL_SMTP_PORT_', 465);
+}
+
+if(!defined('_EMAIL_SMTP_SECURITY_')){
+  define('_EMAIL_SMTP_SECURITY_', 'ssl');
+}
+
+if(!defined('_EMAIL_SMTP_USERNAME_')){
+  define('_EMAIL_SMTP_USERNAME_', '');
+}
+
+if(!defined('_EMAIL_SMTP_PASSWORD_')){
+  define('_EMAIL_SMTP_PASSWORD_', '');
+}
--- lib/autoload.php
+++ lib/autoload.php
@@ -8,9 +8,9 @@ mb_internal_encoding('UTF-8');
 $config_path = __DIR__ . '/../config/config.php';
 if(file_exists($config_path)){
   require_once $config_path;
-}else{
-  require_once './config/config.default.php';
 }
+
+require_once './config/config.default.php';
 
 require_once 'vendor/flight/flight/Flight.php';
 require_once 'functions.php';
